0ca21e0e2a580241227c67b39e48531b207d5f48
Adam <adamwalkerlondon@gmail.com>
2021-11-03 07:50:38 +0000

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import ToDoApp from "./components/ToDoApp";
+
+function App() {
+  return (
+    <div className="App">
+<ToDoApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ToDoApp.js
@@ -0,0 +1,23 @@
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
+
+function ToDoApp() {
+    return (
+      <Paper style={{ /*Paper is a component from material ui imported above.*/
+        padding: 0, 
+        margin: 0, 
+        height: "100vh"}}>
+        <AppBar color="primary" position="static" style={{height: "64px"}}>
+          <Toolbar>
+          <Typography color="inherit">Todo App With with hooks</Typography>
+          </Toolbar>
+        </AppBar>
+      </Paper>
+    );
+  }
+  
+  export default ToDoApp;
+  
