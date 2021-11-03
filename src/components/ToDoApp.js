7a1fa0826f3afdcfee2a34d7efc0e054f401da62
Adam <adamwalkerlondon@gmail.com>
2021-11-03 21:10:15 +0000

    Added setState.

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import AppBar from '@mui/material/AppBar';
@@ -5,6 +6,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Grid from '@mui/material/Grid';
 
 function ToDoApp() {
+
+  const [todos, setTodos] = useState([]);
     return (
       <Paper style={{ /*Paper is a component from material ui imported above.*/
         padding: 0, 
